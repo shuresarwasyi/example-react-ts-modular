3a3fe0288da8f72d9a1d4dbc742903b0b10a0b63
shuresarwasyi <shuresarwasyi@gmail.com>
2023-07-13 07:34:29 +0700

    FIx router issue

--- src/components/member/MemberList.tsx
+++ src/components/member/MemberList.tsx
@@ -18,6 +18,7 @@ const MemberList: React.FC<MemberListProps> = ({ members }) => {
           <Link
             className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-base px-5 py-2.5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 inline-flex"
             to="/"
+            reloadDocument
           >
             Go to Home Page
           </Link>
--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -20,6 +20,7 @@ const HomePage = (): React.JSX.Element => {
             <Link
               className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-base px-5 py-2.5 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 inline-flex"
               to="/member"
+              reloadDocument
             >
               Go to Member Page
             </Link>
